e5e3d4e8e54ea1ed2f96961923627155290f19e8
Sibusky <sibusky@yandex.ru>
2023-03-19 17:13:52 +0400

    feat add routing from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { Layout } from "./pages/layout";
+import { Main } from "./pages/main";
+import { Employee } from "./pages/employee";
+import { Login } from "./pages/login";
+import { PageNotFound } from "./pages/page-not-found";
+
 import "./App.css";
 
-import empoloyeeList from "./db/employees.json";
-
 function App() {
-
-
-
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/">
+          <Route element={<Layout />}>
+            <Route index element={<Main />} />
+            <Route path="edit/:employeeId" element={<Employee />} />
+            <Route path="new" element={<Employee />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }

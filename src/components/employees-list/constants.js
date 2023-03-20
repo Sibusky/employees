3c05cf14b6c2661448a85cfbcc64b81e392ca2a4
Sibusky <sibusky@yandex.ru>
2023-03-20 07:34:51 +0400

    Updates

--- src/components/employees-list/constants.js
+++
@@ -1,5 +0,0 @@
-export const EMPLOYEE_NAME = "Имя сотрудника";
-export const EMPLOYEE_ROLE = "Должность";
-export const EMPLOYEE_PHONE = "Номер телефона";
-export const EMPLOYEE_BIRTHDAY = "Дата рождения";
-export const EMPLOYEE_ISARCHIVED = "В архиве";
--- src/utils/formatDate.js
+++
@@ -1,7 +0,0 @@
-export function formatDate(date) {
-  let dateArr = date.split(".");
-  let day = dateArr[0];
-  let month = dateArr[1];
-  let year = dateArr[2];
-  return [month, day, year].join(".");
-}

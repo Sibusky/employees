e90dddbe68130ed825857b592392fd0f254327aa
Sibusky <sibusky@yandex.ru>
2023-03-21 15:23:43 +0400

    fix: resolve problem with 404 error with gh-pages

--- src/pages/page-not-found.jsx
+++ src/pages/page-not-found.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
 import { Container } from "../components/container";
 import PageNotFoundContent from "../components/page-not-found-content";
 
 export function PageNotFound() {
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  // Это исключительно для GH-Pages
+  useEffect(() => {
+    if (location.pathname === "/employees/" || location.pathname === "/employees") {
+      navigate("/");
+    }
+  }, [navigate, location.pathname]);
+
   return (
     <Container>
       <PageNotFoundContent />

b5dd12ca3a3fde33543b38dfe2de71bea9d7cc21
Sibusky <sibusky@yandex.ru>
2023-03-23 19:46:22 +0400

    feat: add fetch employees with redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import ThunkMiddleware from 'redux-thunk';
 
 import { rootReducer } from './reducers';
-import { employeesMiddleware } from './middleware/employeesMiddleware';
+// import { employeesMiddleware } from './middleware/employeesMiddleware';
+
 
 export const store = configureStore({
     devTools: true,
     reducer: rootReducer,
     middleware: [
-        employeesMiddleware
+        ThunkMiddleware
     ],
 })
+ 
